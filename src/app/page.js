9aef15f65cd57ad9f2e0b3ac4b321706958f6837
DavidJoao <davidsandoval596@gmail.com>
2024-11-17 18:50:43 -0800

    ADDED Redirection on mount

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,15 @@
-export default function Home() {
-  return (
-    <div>
+'use client'
+import { navigate } from "./lib/navigate";
+import { useEffect } from "react";
 
+export default function Home() {
+
+	useEffect(() => {
+		navigate('/home')
+	}, [])
+
+  return (
+    <div className="h-screen grid grid-cols-1 grid-rows-8">
     </div>
   );
 }
