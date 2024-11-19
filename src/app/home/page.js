6dd9ee28d073af35a6beba101552588bcc17e631
DavidJoao <davidsandoval596@gmail.com>
2024-11-19 15:17:11 -0800

    ADDED Trigger to load more books, mapping of books and states

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,6 +1,5 @@
 'use client'
 import { useEffect, useState, useRef } from "react";
-import axios from "axios";
 import Categories from "../components/Categories";
 import Filters from "../components/Filters";
 import BookDetails from "../components/BookDetails";
@@ -9,88 +8,85 @@ import { getBooksData } from "../lib/actions";
 const Home = () => {
 
     const initialFilters = {
-        likes: '',
-        reviews: '',
+        likes: 5,
+        reviews: 5,
+		quantity: 20, 
+		language: 'en'
     }
-
+    
 	const [books, setBooks] = useState([])
-    const [quantity, setQuantity] = useState(20)
-
     const loadMoreTrigger = useRef(null);
     const [loadingBooks, setLoadingBooks] = useState(false);
-    const [language, setLanguage] = useState('en')
-
-
+    const [filters, setFilters] = useState(initialFilters)
 
 	useEffect(() => {
 		const fetchBooks = async () => {
-			const books = await getBooksData(quantity, language);
+			const books = await getBooksData(filters.quantity, filters.language, filters.likes, filters.reviews);
 			setBooks(books);
 		};
 		fetchBooks();
-	}, [])
+	}, [filters]);
 
     useEffect(() => {
-        if (!loadMoreTrigger.current) return;
-    
-        const observer = new IntersectionObserver((entries) => {
-          entries.forEach(entry => {
-            if (entry.isIntersecting && !loadingBooks) {
-              loadMore();
-            }
-          });
-        });
-    
-        observer.observe(loadMoreTrigger.current);
-    
-        return () => {
-          if (loadMoreTrigger.current) observer.unobserve(loadMoreTrigger.current);
-        };
-      }, [loadingBooks, loadMoreTrigger]);
+		if (!loadMoreTrigger.current) return
 
-    const loadMore = async () => {
-        setLoadingBooks(true);
-        const newQuantity = quantity + 20;
-        setQuantity(newQuantity)
+		const observer = new IntersectionObserver(entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting && !loadingBooks) {
+					loadMore()
+				}
+			})
+		})
+		observer.observe(loadMoreTrigger.current)
+		return () => {
+			if (loadMoreTrigger.current) observer.unobserve(loadMoreTrigger.current)
+		}
+	}, [loadingBooks, loadMoreTrigger])
 
-        try {
-            const newData = await getBooksData(newQuantity, language);
-            setBooks(prevBooks => [...prevBooks, ...newData]);
-        } catch (error) {
-            console.log(error);
-        } finally {
-            setTimeout(() => {
-                setLoadingBooks(false)
-        }, 1000)
-        }
-    }
+	const loadMore = async () => {
+		setLoadingBooks(true)
+		const newQuantity = filters?.quantity + 20
+		setFilters({...filters, ['quantity']: newQuantity})
+
+		try {
+			const newData = await getBooksData(newQuantity, filters?.language, filters?.likes, filters?.reviews)
+			setBooks(prevBooks => [...prevBooks, ...newData])
+		} catch (error) {
+			console.log(error)
+		} finally {
+			setTimeout(() => {
+				setLoadingBooks(false)
+			}, 1000)
+		}
+	}
 
 	return (
 		<div className="h-screen grid grid-cols-1 grid-rows-8">
-            { books ? (
-                <>
-                <div className="border h-full grid grid-cols-5 grid-rows-2 p-2 pb-0">
-                    <Filters language={language} setLanguage={setLanguage} setBooks={setBooks} quantity={quantity} setQuantity={setQuantity}/>
-                    <Categories />
-                </div>
-                <div className="border-[1px] border-black h-full row-span-7 grid overflow-auto">
-                    {books &&
-                        books?.map((book, index) => {
-                            return (
-                                <div key={index} className="border-[1px] border-black p-2 grid grid-cols-5">
-                                    <BookDetails index={index} book={book} />
-                                </div>
-                            )
-                        })}
-                    {loadingBooks && <p className="text-center h-[20px]">Loading more data...</p>}
-                    <div ref={loadMoreTrigger} className="h-[20px]"/>
-                </div>
-                </>
-            ) : (
-                <>
-                <p>loading</p>
-                </>
-            )}
+			{books ? (
+				<>
+					<div className="border h-full grid grid-cols-5 grid-rows-2 p-2 pb-0">
+						<Filters setBooks={setBooks} filters={filters} setFilters={setFilters} />
+						<Categories />
+					</div>
+					<div className="border-[1px] border-black h-full row-span-7 grid overflow-auto">
+						{books && books.map((book, index) => {
+							return (
+								<div key={index} className="border-[1px] border-black p-2 grid grid-cols-5">
+									<BookDetails index={index} book={book} />
+								</div>
+								)
+						})}
+						{loadingBooks && (
+							<p className="text-center h-[20px]">Loading more data...</p>
+						)}
+						<div ref={loadMoreTrigger} className="h-[20px]" />
+					</div>
+				</>
+			) : (
+				<>
+					<p>loading</p>
+				</>
+			)}
 		</div>
 	)
 }
