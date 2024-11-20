2951b24e8907565379f7e2a7bdb6cf54863bb13c
DavidJoao <davidsandoval596@gmail.com>
2024-11-19 16:02:57 -0800

    ADDED Initial Modal

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -4,30 +4,37 @@ import Categories from "../components/Categories";
 import Filters from "../components/Filters";
 import BookDetails from "../components/BookDetails";
 import { getBooksData } from "../lib/actions";
+import { Modal } from "react-bootstrap";
 
 const Home = () => {
+	const initialFilters = {
+		likes: 5,
+		reviews: 5,
+		quantity: 20,
+		language: "en",
+	}
 
-    const initialFilters = {
-        likes: 5,
-        reviews: 5,
-		quantity: 20, 
-		language: 'en'
-    }
-    
 	const [books, setBooks] = useState([])
-    const loadMoreTrigger = useRef(null);
-    const [loadingBooks, setLoadingBooks] = useState(false);
-    const [filters, setFilters] = useState(initialFilters)
+	const loadMoreTrigger = useRef(null)
+	const [loadingBooks, setLoadingBooks] = useState(false)
+	const [filters, setFilters] = useState(initialFilters)
+	const [modalState, setModalState] = useState(false)
+	const [selectedBook, setSelectedBook] = useState({})
 
 	useEffect(() => {
 		const fetchBooks = async () => {
-			const books = await getBooksData(filters.quantity, filters.language, filters.likes, filters.reviews);
-			setBooks(books);
-		};
-		fetchBooks();
-	}, [filters]);
+			const books = await getBooksData(
+				filters.quantity,
+				filters.language,
+				filters.likes,
+				filters.reviews
+			)
+			setBooks(books)
+		}
+		fetchBooks()
+	}, [filters])
 
-    useEffect(() => {
+	useEffect(() => {
 		if (!loadMoreTrigger.current) return
 
 		const observer = new IntersectionObserver(entries => {
@@ -46,10 +53,15 @@ const Home = () => {
 	const loadMore = async () => {
 		setLoadingBooks(true)
 		const newQuantity = filters?.quantity + 20
-		setFilters({...filters, ['quantity']: newQuantity})
+		setFilters({ ...filters, ["quantity"]: newQuantity })
 
 		try {
-			const newData = await getBooksData(newQuantity, filters?.language, filters?.likes, filters?.reviews)
+			const newData = await getBooksData(
+				newQuantity,
+				filters?.language,
+				filters?.likes,
+				filters?.reviews
+			)
 			setBooks(prevBooks => [...prevBooks, ...newData])
 		} catch (error) {
 			console.log(error)
@@ -69,13 +81,20 @@ const Home = () => {
 						<Categories />
 					</div>
 					<div className="border-[1px] border-black h-full row-span-7 grid overflow-auto">
-						{books && books.map((book, index) => {
-							return (
-								<div key={index} className="border-[1px] border-black p-2 grid grid-cols-5">
-									<BookDetails index={index} book={book} />
-								</div>
+						{books &&
+							books.map((book, index) => {
+								return (
+									<div key={index} className="border-[1px] border-black p-2 grid grid-cols-5" onClick={() => {
+											setSelectedBook(book)
+											setModalState(true)
+										}}>
+										<BookDetails index={index} book={book} modalState={modalState} setModalState={setModalState} />
+									</div>
 								)
-						})}
+							})}
+						<Modal show={modalState} onHide={() => setModalState(false)}>
+							<Modal.Header closeButton>{selectedBook?.title}</Modal.Header>
+						</Modal>
 						{loadingBooks && (
 							<p className="text-center h-[20px]">Loading more data...</p>
 						)}
