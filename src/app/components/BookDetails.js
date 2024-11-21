1ec002b4f0f469955ddb9949319971e2cfa34b28
DavidJoao <davidsandoval596@gmail.com>
2024-11-20 20:25:14 -0800

    CHANGED Categories classes

--- src/app/components/BookDetails.js
+++ src/app/components/BookDetails.js
@@ -3,11 +3,11 @@ import React from 'react'
 const BookDetails = ( {index, book} ) => {
   return (
     <>
-      <p className='text-center'>{index + 1}</p>
-      <p className='text-center'>{book?.ISBN}</p>
-      <p className='text-center'>{book?.title}</p>
-      <p className='text-center'>{book?.author}</p>
-      <p className='text-center'>{book?.publisher}</p>
+      <p className='text-center flex items-center justify-center w-[200px]'>{index + 1 || '#'}</p>
+      <p className='text-center flex items-center justify-center w-[200px]'>{book?.ISBN}</p>
+      <p className='text-center flex items-center justify-center w-[200px]'>{book?.title}</p>
+      <p className='text-center flex items-center justify-center w-[200px]'>{book?.author}</p>
+      <p className='text-center flex items-center justify-center w-[200px]'>{book?.publisher}</p>
     </>
   )
 }
