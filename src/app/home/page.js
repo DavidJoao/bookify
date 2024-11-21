77216c287bb3369fe0630e1c5cc02bd6e0f29480
DavidJoao <davidsandoval596@gmail.com>
2024-11-20 20:27:29 -0800

    ADDED Seed change handler and height calculation function

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -5,12 +5,14 @@ import Filters from "../components/Filters";
 import BookDetails from "../components/BookDetails";
 import { getBooksData } from "../lib/actions";
 import { Modal } from "react-bootstrap";
+import seedrandom from "seedrandom";
+import Image from "next/image";
 
 const Home = () => {
 	const initialFilters = {
 		likes: 5,
 		reviews: 5,
-		quantity: 20,
+		quantity: 40,
 		language: "en",
 	}
 
@@ -20,6 +22,20 @@ const Home = () => {
 	const [filters, setFilters] = useState(initialFilters)
 	const [modalState, setModalState] = useState(false)
 	const [selectedBook, setSelectedBook] = useState({})
+	const [toolbarHeight, setToolbarHeight] = useState(0)
+	const [seed, setSeed] = useState(50)
+	const [view, setView] = useState("list")	
+
+	useEffect(() => {
+		const toolbar = document.getElementById("toolbar")
+		if (toolbar) {
+			setToolbarHeight(toolbar.offsetHeight)
+			document.documentElement.style.setProperty(
+				"--toolbar-height",
+				`${toolbar.offsetHeight}px`
+			)
+		}
+	}, [])
 
 	useEffect(() => {
 		const fetchBooks = async () => {
@@ -72,39 +88,137 @@ const Home = () => {
 		}
 	}
 
+	function getBookBySeed(seed) {
+		const rng = seedrandom(seed)
+		const randomIndex = Math.floor(rng() * books.length)
+		return books[randomIndex]
+	}
+
+	const handleSeedChange = async seed => {
+		setSeed(seed)
+		setSelectedBook(getBookBySeed(seed))
+		setModalState(true)
+	}
+
 	return (
-		<div className="h-screen grid grid-cols-1 grid-rows-8">
+		<div className="flex flex-col items-center justify-center">
+		<Modal show={modalState} onHide={() => setModalState(false)}>
+			<Modal.Header className="font-bold text-[20px]" closeButton>{selectedBook?.title} by {selectedBook.author}</Modal.Header>
+			<Modal.Body>
+				<div className="h-[300px] w-[200px] mx-auto rounded" style={{ background:(`url("${selectedBook.image}")`), backgroundSize:'cover' }}></div>
+				<div className="flex flex-col m-2 items-center justify-evenly">
+					<p>Publisher: {selectedBook.publisher}</p>
+					<p>Likes</p>
+				</div>
+				<div>
+					<p className="text-center mt-3 bg-slate-200 p-1 rounded-t font-bold shadow-lg">Reviews: {selectedBook?.reviews?.length}</p>
+					<div className="border h-[400px] md:h-[250px] overflow-auto rounded-b">
+						{ selectedBook?.reviews?.map((review, index) => {
+							return (
+							<div key={index} className="border p-2 hover:bg-slate-100">
+								<p className="font-bold">{review?.name}</p>
+								<p className="text-slate-400 italic text-sm">- {review?.review}</p>
+							</div>
+							)
+						}) }
+					</div>
+				</div>
+			</Modal.Body>
+		</Modal>
 			{books ? (
 				<>
-					<div className="border h-full grid grid-cols-5 grid-rows-2 p-2 pb-0">
-						<Filters setBooks={setBooks} filters={filters} setFilters={setFilters} />
-						<Categories />
+					{/* // Toolbar Div */}
+					<div className="w-full h-auto flex-col static" id="toolbar">
+						<div className="w-full h-auto flex flex-col md:flex-row gap-2 p-2">
+							<Filters
+								setBooks={setBooks}
+								filters={filters}
+								setFilters={setFilters}
+								handleSeedChange={handleSeedChange}
+								books={books}
+								setSeed={setSeed}
+								setSelectedBook={setSelectedBook}
+								setModalState={setModalState}
+								getBookBySeed={getBookBySeed}
+								view={view}
+								setView={setView}
+							/>
+						</div>
+						<div className="hidden md:flex flex-row items-center justify-around p-1 shadow-xl">
+							<Categories />
+						</div>
 					</div>
-					<div className="border-[1px] border-black h-full row-span-7 grid overflow-auto">
-						{books &&
-							books.map((book, index) => {
+
+					{view === "list" ? (
+						<div className="w-full h-[calc(100vh-var(--toolbar-height))] w-full overflow-x-auto overflow-y-auto flex flex-col px-auto">
+							<div className="p-2 grid grid-cols-5 md:hidden p-2 w-[120vh] md:w-full sticky top-0 bg-white">
+								<BookDetails
+									book={{
+										index: "#",
+										ISBN: "ISBN",
+										title: "Title",
+										author: "Author(s)",
+										publisher: "Publisher",
+									}}
+								/>
+							</div>
+							{books &&
+								books.map((book, index) => {
+									return (
+										<div
+											key={index}
+											className="grid grid-cols-5 p-2 shadow-lg w-[120vh] md:w-full"
+											onClick={() => {
+												setSelectedBook(book)
+												setModalState(true)
+											}}>
+											<BookDetails
+												index={index}
+												book={book}
+												modalState={modalState}
+												setModalState={setModalState}
+											/>
+										</div>
+									)
+								})}
+							<div
+								className="p-2 grid grid-cols-5 p-2 w-[120vh] md:w-full"
+								ref={loadMoreTrigger}>
+								<BookDetails
+									book={{
+										index: "Loading",
+										ISBN: "Loading",
+										title: "Loading",
+										author: "Loading",
+										publisher: "Loading",
+									}}
+								/>
+							</div>
+							{loadingBooks && (
+								<p className="text-center h-[20px]">Loading more data...</p>
+							)}
+						</div>
+					) : (
+						<div className="w-full h-[calc(100vh-var(--toolbar-height))] grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 xl:grid-cols-10 gap-3 p-3">
+							{ books.map((book, index) => {
 								return (
-									<div key={index} className="border-[1px] border-black p-2 grid grid-cols-5" onClick={() => {
+									<>
+										<div className="w-[150px] h-[200px] brightness-50 hover:brightness-100 flex flex-col items-center justify-center rounded p-3" key={index} style={{ background:`url('${book.image}')`, backgroundSize:"cover"}} onClick={() => {
 											setSelectedBook(book)
 											setModalState(true)
 										}}>
-										<BookDetails index={index} book={book} modalState={modalState} setModalState={setModalState} />
-									</div>
+											<p className="font-bold text-white brightness-150 text-center">{book.title}</p>
+											<p className="font-bold text-white brightness-150 text-center">By: {book.author}</p>
+										</div>
+									</>
 								)
-							})}
-						<Modal show={modalState} onHide={() => setModalState(false)}>
-							<Modal.Header closeButton>{selectedBook?.title}</Modal.Header>
-						</Modal>
-						{loadingBooks && (
-							<p className="text-center h-[20px]">Loading more data...</p>
-						)}
-						<div ref={loadMoreTrigger} className="h-[20px]" />
-					</div>
+							}) }
+							<div className="w-[150px] h-[200px] brightness-50 hover:brightness-100 flex items-center justify-center rounded p-3 border" ref={loadMoreTrigger}></div>
+						</div>
+					)}
 				</>
 			) : (
-				<>
-					<p>loading</p>
-				</>
+				<p>Loading Books...</p>
 			)}
 		</div>
 	)
