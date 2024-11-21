63801ba72e24ed97948b8635b20744cb2dc012ab
DavidJoao <davidsandoval596@gmail.com>
2024-11-21 15:43:44 -0800

    CREATED Action exportBooksData that generates a csv of current books

--- src/app/lib/actions.js
+++ src/app/lib/actions.js
@@ -8,3 +8,19 @@ export const getBooksData = async (quantity, language, likes, reviews) => {
         console.error("Error fetching books data", error)
     }
 }
+
+export const exportBooksData = async (books) => {
+    try {
+        const res = await axios.post("/api/export", books, { headers: { 'Content-Type':'application/json' } })
+        if (res.status === 200) {
+            const blob = new Blob([res.data], { type: 'text/csv' });
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = 'books.csv';
+            link.click();
+        }
+        return res;
+    } catch (error) {
+        console.error("Error exporting books", error)
+    }
+}
