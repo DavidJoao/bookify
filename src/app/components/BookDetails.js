72195e1a6c125f7b98530f54033432c35fa654a2
DavidJoao <davidsandoval596@gmail.com>
2024-11-22 17:17:42 -0800

    CHANGED Margin for book details tab

--- src/app/components/BookDetails.js
+++ src/app/components/BookDetails.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { chevronDownIcon, chevronUpIcon } from '../lib/icons'
 
-const BookDetails = ( {index, book} ) => {
+const BookDetails = ( {index, book, selectedBook} ) => {
   return (
     <>
-      <p className='text-center flex items-center justify-center w-[200px]'>{index + 1 || '#'}</p>
-      <p className='text-center flex items-center justify-center w-[200px]'>{book?.ISBN}</p>
-      <p className='text-center flex items-center justify-center w-[200px]'>{book?.title}</p>
-      <p className='text-center flex items-center justify-center w-[200px]'>{book?.author}</p>
-      <p className='text-center flex items-center justify-center w-[200px]'>{book?.publisher}</p>
+      <p className='text-center flex items-center justify-center w-[200px] m-2 font-bold'>{selectedBook?.ISBN === book?.ISBN ? chevronUpIcon : chevronDownIcon} {index + 1 || "#"} </p>
+      <p className='text-center flex items-center justify-center w-[200px] m-2'>{book?.ISBN}</p>
+      <p className='text-center flex items-center justify-center w-[200px] m-2'>{book?.title}</p>
+      <p className='text-center flex items-center justify-center w-[200px] m-2'>{book?.author}</p>
+      <p className='text-center flex items-center justify-center w-[200px] m-2'>{book?.publisher}</p>
     </>
   )
 }
